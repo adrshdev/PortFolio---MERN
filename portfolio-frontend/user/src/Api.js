d6015c0d61fc767765b7b49aaf4272789f9fa4ff
adrshdev <adarshjayasankerdev@gmail.com>
2025-08-12 22:34:46 +0530

    solved a spelling mistake in code

--- portfolio-frontend/user/src/Api.js
+++ portfolio-frontend/user/src/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Api = axios.create({
-  baseURL: ' https://portfolio-mern-2-d3j1.onrender.com',
+  baseURL: 'https://portfolio-mern-1-s1oo.onrender.com',
   withCredentials: true,
 });
 
